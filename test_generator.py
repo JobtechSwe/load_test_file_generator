934530dce3e8daa430e32e8fec9310eacb4a53d0
Erik Melander <erik.melander@arbetsformedlingen.se>
2019-02-06 09:47:53 +0100

    first commit

---
+++ test_generator.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+def import_counties():
+    r = requests.get('https://sokannonser.dev.services.jtech.se/vf/search?offset=0&limit=100&type=county', headers={'api-key': 'apa'})
+    data = r.json()
+    lan = data['result']
+    return lan
+
+
+def import_municipalities():
+    r = requests.get('https://sokannonser.dev.services.jtech.se/vf/search?offset=0&limit=1000&type=municipality', headers={'api-key': 'apa'})
+    data = r.json()
+    muni = data['result']
+    return muni
+
+
+def generate_municipalities_file(muni):
+    payload = {"version": 1,
+               "variables": [{
+                   "names": ["municipality"],
+                   "values": []
+               }
+               ]}
+    m_list = []
+    for m in muni:
+        m_list.append(m['legacyAmsTaxonomyId'])
+    payload['variables'][0]['values'] = m_list
+    return payload
+
+
+def to_file(j, filename):
+    with open(filename, "w") as fp:
+        json.dump(j, fp)
+
+
+if __name__ == "__main__":
+    p = generate_municipalities_file(import_municipalities())
+    to_file(p, "test.json")
